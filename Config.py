c1039fdef137641242ab74a160b89f570f21416d
Stark Bots <88478059+StarkBotsIndustries@users.noreply.github.com>
2021-08-05 16:49:24 +0530

    Check for Integer

--- Config.py
+++ Config.py
@@ -3,7 +3,10 @@ import os
 ENVIRONMENT = bool(os.environ.get('ENVIRONMENT', False))
 
 if ENVIRONMENT:
-    API_ID = int(os.environ.get('API_ID', 0))
+    try:
+        API_ID = int(os.environ.get('API_ID', 0))
+    except ValueError:
+        raise Exception("Your API_ID is not a valid integer.")
     API_HASH = os.environ.get('API_HASH', None)
     BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
 else:
